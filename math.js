67059c9fb23175e3a65520d331e61e53edcf1c2e
Richard Seward <raseward14@gmail.com>
2022-07-15 07:59:09 -0600

    created math.js

---
+++ math.js
@@ -0,0 +1,8 @@
+// four functions
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// have not exported them in any way 13:07
