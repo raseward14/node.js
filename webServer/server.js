bc963bf332ef1c545d198c64c0e3f9af57ef52f8
Richard Seward <raseward14@gmail.com>
2022-07-26 13:19:50 -0600

    tsing filePath

--- webServer/server.js
+++ webServer/server.js
@@ -11,15 +11,40 @@ const EventEmitter = require('events');
 class Emitter extends EventEmitter {};
 // initialize object
 const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 // STEP 2: DEVINE PORT FOR WEB SERVER
 // will have address of localhost bc it will just be a dev server on our local machine, we need to say what port it will be on
 // process.env.PORT - if we were to host this somewhere, it would use this information, OR 3500;
 const PORT = process.env.PORT || 3500;
 
-// STEP 3: CREATE MINIMAL SERVER
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+// serve file function
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(
+            filePath, 
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200, 
+            { 'Content-Type': contentType });
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+            );
+        } catch(err) {
+            console.log(err)
+            myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
+            response.statusCode = 500;
+            response.end();
+        }
+    }
+    
+    // STEP 3: CREATE MINIMAL SERVER
+    const server = http.createServer((req, res) => {
+        console.log(req.url, req.method);
+        myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
 
     // gets the extension - / wouldnt have an extension, but we can handle that as well
     const extension = path.extname(req.url)
@@ -44,11 +69,15 @@ const server = http.createServer((req, res) => {
     }
 
     let filePath =
-        contentType === 'text/html' && req.url === '/' ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/' ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html' ? path.join(__dirname, 'views', req.url)
+        contentType === 'text/html' && req.url === '/' 
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/' 
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html' 
+                    ? path.join(__dirname, 'views', req.url)
                     : path.join(__dirname, req.url)
-
+console.log(filePath)
+//left at 2 hours
     // makes .html extension not required in browser
     if(!extension && req.url.slice(-1) !== '/') filePath += '.html'
 
@@ -56,34 +85,26 @@ const server = http.createServer((req, res) => {
     const fileExists = fs.existsSync(filePath);
 
     if(fileExists) {
-        // serve the file
+        // serve the file with a function
+        serveFile(filePath, contentType, res)
     } else {
         // 404
         // 301 redirect
-        console.log(path.parse(filePath));
+        // console.log(path.parse(filePath));
+        switch(path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, {'Location': '/new-page.html' })
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, {'Location': '/' })
+                res.end();
+                break;
+            default:
+                // serve a 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
     }
-    // 1:42:22
-
-    // let path;
-
-    // if(req.url === '/' || req.url === 'index.html') {
-    //     res.statusCode = 200;
-    //     res.setHeader('Content-Type', 'text/html');
-    //     pathh = path.join(__dirname, 'views', 'index.html');
-    //     fs.readFile(path, 'utf8', () => {
-    //         res.send(data)
-    //     })
-    // }
-
-    // switch(req.url) {
-    //     case '/':
-    //         res.statusCode = 200;
-    //         path = path.join(__dirname, 'views', 'index.html');
-    //         fs.readFile(path, 'utf8', (err, data) => {
-    //             res.send(data)
-    //         })
-    //         break;
-    // }
 });
 
 // STEP 4: LISTEN FOR REQUESTS - always at end of server.js file
@@ -93,7 +114,5 @@ server.listen(PORT, () => {
 });
 // visiting localhost:3500 wil show /GET - left at 1:31:09
 
-// myEmitter.on('log', (msg) => logEvents(msg));
 
 
-//     myEmitter.emit('log', 'Log event emitted!')
