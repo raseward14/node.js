f11c4a06824997c4f46b55064459cdfbfdb3ee06
Richard Seward <raseward14@gmail.com>
2022-07-27 07:26:54 -0600

    fixed switch by adding breaks

--- webServer/server.js
+++ webServer/server.js
@@ -2,13 +2,13 @@
 // import common core modules needed for this file here
 const http = require('http');
 const path = require('path');
-const fs = require ('fs');
+const fs = require('fs');
 const fsPromises = require('fs').promises;
 
 // custom module, not a common core module, or an npm module, so we need to define the path
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
+class Emitter extends EventEmitter { };
 // initialize object
 const myEmitter = new Emitter();
 myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
@@ -22,29 +22,29 @@ const PORT = process.env.PORT || 3500;
 const serveFile = async (filePath, contentType, response) => {
     try {
         const rawData = await fsPromises.readFile(
-            filePath, 
+            filePath,
             !contentType.includes('image') ? 'utf8' : ''
         );
         const data = contentType === 'application/json'
             ? JSON.parse(rawData) : rawData;
         response.writeHead(
-            filePath.includes('404.html') ? 404 : 200, 
+            filePath.includes('404.html') ? 404 : 200,
             { 'Content-Type': contentType });
         response.end(
             contentType === 'application/json' ? JSON.stringify(data) : data
-            );
-        } catch(err) {
-            console.log(err)
-            myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
-            response.statusCode = 500;
-            response.end();
-        }
+        );
+    } catch (err) {
+        console.log(err)
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
+        response.statusCode = 500;
+        response.end();
     }
-    
-    // STEP 3: CREATE MINIMAL SERVER
-    const server = http.createServer((req, res) => {
-        console.log(req.url, req.method);
-        myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
+}
+
+// STEP 3: CREATE MINIMAL SERVER
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
 
     // gets the extension - / wouldnt have an extension, but we can handle that as well
     const extension = path.extname(req.url)
@@ -54,50 +54,54 @@ const serveFile = async (filePath, contentType, response) => {
     switch (extension) {
         case '.css':
             contentType = 'text/css';
+            break;
         case '.js':
             contentType = 'text/javascript';
+            break;
         case '.json':
             contentType = 'application/json';
+            break;
         case '.jpg':
             contentType = 'image/jpeg';
+            break;
         case '.png':
             contentType = 'image/png';
+            break;
         case '.txt':
             contentType = 'text/plain';
+            break;
         default:
             contentType = 'text/html';
     }
 
     let filePath =
-        contentType === 'text/html' && req.url === '/' 
+        contentType === 'text/html' && req.url === '/'
             ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/' 
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
                 ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html' 
+                : contentType === 'text/html'
                     ? path.join(__dirname, 'views', req.url)
                     : path.join(__dirname, req.url)
-console.log(filePath)
-//left at 2 hours
     // makes .html extension not required in browser
-    if(!extension && req.url.slice(-1) !== '/') filePath += '.html'
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
 
     // file exists or not, true or false value we get back
     const fileExists = fs.existsSync(filePath);
 
-    if(fileExists) {
+    if (fileExists) {
         // serve the file with a function
         serveFile(filePath, contentType, res)
     } else {
         // 404
         // 301 redirect
         // console.log(path.parse(filePath));
-        switch(path.parse(filePath).base) {
+        switch (path.parse(filePath).base) {
             case 'old-page.html':
-                res.writeHead(301, {'Location': '/new-page.html' })
+                res.writeHead(301, { 'Location': '/new-page.html' })
                 res.end();
                 break;
             case 'www-page.html':
-                res.writeHead(301, {'Location': '/' })
+                res.writeHead(301, { 'Location': '/' })
                 res.end();
                 break;
             default:
