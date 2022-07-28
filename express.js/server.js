7b0c9b601a492a664503ed355c28c180cc5bd4b3
Richard Seward <raseward14@gmail.com>
2022-07-28 07:58:50 -0600

    express route handling

--- express.js/server.js
+++ express.js/server.js
@@ -3,10 +3,38 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
+// must begin with a slash, end with a slash, or be index.html - express supports regular expressions, the .html is optional
+app.get('^/$|index(.html)?', (req, res) => {
     // res.send('hello world!');
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+app.get('/new-page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    // 301 this has permenantly been moved to new page, there is no more old page
+    res.redirect(301, '/new-page.html'); // 302 by default, we want a 301
+});
+
+// a '/' followed by anything will default to this route
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // express will send a 404 to tell you it cant find what its looking for, even if we dont supply a code
+    // it wont send a 404 status code, it will send a 200 bc its actually successfully finding our 404 page, serving exactly what we told it to
+    // chain in the .status(404) method
+})
+
+//2:21:00
+// route handlers
+// what it does is it moves on to the next handler, or the next expression - not common
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('hello world!')
+})
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
