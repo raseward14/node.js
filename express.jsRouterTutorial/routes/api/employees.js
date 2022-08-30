6160a2997523533854f893fa31d93b83d71e81e1
Richard Seward <raseward14@gmail.com>
2022-08-30 07:56:42 -0600

    started API

---
+++ express.jsRouterTutorial/routes/api/employees.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+// just for this tutorial, importing json data
+const data = {};
+data.employees = require('../../data/employees.json')
+
+// we can create the route, and chain the http methods we want to provide for this route
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees)
+    })
+    .post((req, res) => {
+        // can refer to those parameters with req.body. whatever the name of the paramater is
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        })
+    })
+    .put((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname
+        })
+    })
+    .delete((req, res) => {
+        res.json({ "id": req.body.id })
+    });
+
+// can have a parameter directly within the URL
+router.route('/:id')
+    .get((req, res) => {
+        // refer to the named parameter with req.params. whatever the name of the parameter is
+        res.json({ "id": req.params.id })
+    })
+
+module.exports = router;
--- express.jsRouterTutorial/server.js
+++ express.jsRouterTutorial/server.js
@@ -35,7 +35,9 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'))
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
 
 app.all('*', (req, res) => {
     res.status(404);
